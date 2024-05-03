e8191757f7b8fcd9fcf1a2e4e56ee37de1d11172
Alexander Sullivan <alexander.j.sullivan@hotmail.com>
2024-05-03 19:01:14 -0400

    Update Service Worker files

--- workbox-config.js
+++ workbox-config.js
@@ -1,32 +1,33 @@
 module.exports = {
-  "globDirectory": "./src/",
-  "globPatterns": [
-    "**/*.{html,json,jpg,png,mp4,gif,xml,ico,svg,webmanifest,js,css,webp}"
-  ],
-  "swDest": "./src/serviceWorker/sw.js",
-  "globIgnores": [
-    'index.html',
-    'workbox-config.js',
-    'package.json',
-    'package-lock.json'
-  ],
+	"globDirectory": "./src/",
+	"globPatterns": [
+		"**/*.{html,json,jpg,png,mp4,gif,xml,ico,svg,webmanifest,js,css,webp}"
+	],
+	"swDest": "./src/serviceWorker/sw.js",
+	"globIgnores": [
+		'index.html',
+		'workbox-config.js',
+		'package.json',
+		'package-lock.json',
+		'node_modules/**/*',
+	],
 
-  // Define runtime caching rules.
-  "runtimeCaching": [{
-    // Match any request that ends with .png, .jpg, .jpeg, .webp, .ico or .svg
-    "urlPattern": /\.(?:png|jpg|jpeg|webp|ico|svg)$/,
+	// Define runtime caching rules.
+	"runtimeCaching": [{
+		// Match any request that ends with .png, .jpg, .jpeg, .webp, .ico or .svg
+		"urlPattern": /\.(?:png|jpg|jpeg|webp|ico|svg)$/,
 
-    // Apply a cache-first strategy.
-    "handler": 'CacheFirst',
+		// Apply a cache-first strategy.
+		"handler": 'CacheFirst',
 
-    "options": {
-      // Use a custom cache name.
-      "cacheName": 'images',
+		"options": {
+			// Use a custom cache name.
+			"cacheName": 'images',
 
-      // Only cache 10 images.
-      "expiration": {
-        "maxEntries": 10,
-      },
-    },
-  }],
+			// Only cache 10 images.
+			"expiration": {
+				"maxEntries": 10,
+			},
+		},
+	}],
 };
